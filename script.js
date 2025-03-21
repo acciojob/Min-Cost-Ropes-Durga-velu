48e52659b7a19beb5d7f65e510345cfd83d76a7e
Durga-velu <durgadevi134babu@gmail.com>
2025-03-22 01:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,24 +37,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-let sum =0;
-let res =0;
-while(arr.length!=1)
+let result=0;
+while(arr.length>1)
 { 
 arr.sort((a,b)=>
 {
-					if(a>b)
-						return b-a;
-					else return a - b;
-					return a -b;
+     return a -b;
 });
-let sum = arr[0]+arr[1];
-res +=sum;
-arr.splice(0,2,sum);
-arr.sort();
-// res+=arr[0];
+let first= arr.shift();
+	let second=arr.shift();
+
+	let sum=first+second
+	result +=sum;
+	arr.push(sum)
 }
-return res;
+return result;
 }
 
 module.exports=mincost;
