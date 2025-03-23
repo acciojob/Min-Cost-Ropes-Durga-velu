fd7b32348cd307fb10a576bc2c6c30363e88dd3a
Durga-velu <durgadevi134babu@gmail.com>
2025-03-23 13:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// let PriorityQueue = require('priorityqueuejs');
-// function mincost(arr) {
-
-// if (arr.length===0) {
-// 	return 0
-// }
-	
-//   let minHeap = new PriorityQueue(function(a, b) {
-//     return a - b;
-//   });
-//   for(let i = 0; i < arr.length; i++) {
-//     minHeap.enq(arr[i]);
-//   }
-//   let totalCost = 0;
-//   while(minHeap.size() > 1) {
-//     let cost = minHeap.deq() + minHeap.deq();
-//     totalCost += cost;
-//     minHeap.enq(cost);
-//   }
-//   return totalCost;
-// }
-//  //module.exports=mincost;
-
-
-// let arr=[4,3,2,6]
-
-// console.log(mincost(arr))
-
-
-
-
-
-
-
-
 function mincost(arr)
 { 
 //write your code here
